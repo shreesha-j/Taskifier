e3afacc17b90ef5da81f1a17aae3bd9e5d927dc9
shreesha-j <shreesha1005@gmail.com>
2024-11-01 01:50:55 +0000

    feat(client): added some best practices

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,55 +1,47 @@
-import React from "react";
-import { Box } from "@mui/material";
-import { useState,useEffect } from "react";
-import { Outlet,useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from 'react';
+import { Box } from '@mui/material';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
-import authUtils from "../../utils/authUtils";
-import Loading from "../../common/Loading";
-import { setUser } from "../../redux/features/userSlice";
+import authUtils from '../../utils/authUtils';
+import Loading from '../../common/Loading';
+import Sidebar from '../../common/Sidebar';
+import { setUser } from '../../redux/features/userSlice';
 
 /**
  * AppLayout component for the main application layout.
- * Displays a loading spinner initially and renders the main content once loading is complete.
+ * Displays a loading spinner initially and renders the main content once authentication is verified.
  *
  * @returns {JSX.Element} The main application layout with conditional loading.
  */
 const AppLayout = () => {
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const checkAuth = async () => {
-        const User = await authUtils.isAuthenticated();
-        if (!User) {
-            navigate('/login')
-        } else {
-            dispatch(setUser(User))
-            setLoading(false)
-        }
-    }
-    checkAuth()
-} ,[navigate])
+      const user = await authUtils.isAuthenticated();
+      if (!user) {
+        navigate('/login');
+      } else {
+        dispatch(setUser(user));
+        setLoading(false);
+      }
+    };
+    checkAuth();
+  }, [dispatch, navigate]);
 
-return (
-  loading ? ( <Loading fullHeight/>)
-  : (
-
-<Box sx={{
-  display: 'flex',
-  }}>
-  <Sidebar />
-  <Box sx={{
-      flexGrow:1,
-      p:1,
-      width:'max-content'
-  }}>
-  <Outlet />
-  </Box>
-</Box>
-)
-);
+  return loading ? (
+    <Loading fullHeight />
+  ) : (
+    <Box sx={{ display: 'flex' }}>
+      <Sidebar />
+      <Box sx={{ flexGrow: 1, p: 1, width: 'max-content' }}>
+        <Outlet />
+      </Box>
+    </Box>
+  );
 };
 
 export default AppLayout;
--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -1,52 +1,49 @@
-import React from "react";
-import { Box, Container } from "@mui/material";
-import { useState,useEffect } from "react";
-import { Outlet,useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import { Box, Container } from '@mui/material';
+import { Outlet, useNavigate } from 'react-router-dom';
 
-import authUtils from "../../utils/authUtils";
-import Loading from "../../common/Loading";
-import Assets from "../../assets";
+import authUtils from '../../utils/authUtils';
+import Loading from '../../common/Loading';
+import Assets from '../../assets';
 
 /**
  * AuthLayout component for the authentication pages layout.
- * Displays a loading spinner initially and shows the logo and content once loading is complete.
+ * Displays a loading spinner initially and shows the logo and content once authentication status is verified.
  *
  * @returns {JSX.Element} The authentication layout with conditional loading.
  */
 const AuthLayout = () => {
-  const navigate = useNavigate()
-    const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const checkAuth = async () => {
-            const isAuthenticated = await authUtils.isAuthenticated();
-            if (!isAuthenticated) {
-                setLoading(false)
-            } else {
-                navigate('/')
-            }
-        }
-        checkAuth() 
-    } ,[navigate])
+  useEffect(() => {
+    const checkAuth = async () => {
+      const isAuthenticated = await authUtils.isAuthenticated();
+      if (!isAuthenticated) {
+        setLoading(false);
+      } else {
+        navigate('/');
+      }
+    };
+    checkAuth();
+  }, [navigate]);
 
-    return (
-      loading ? ( <Loading fullHeight/>)
-      : (
-      <Container
-          component='main' maxWidth='xs'
-          >
-      <Box 
-          sx={{
-              marginTop : 8,
-              display: 'flex',
-              alignItems: 'center',
-              flexDirection: 'column',
-          }}>
-              <img src={Assets.Images.logo} alt="logo" style={{ width: '100px' }} />
-              <Outlet />
-          </Box>
-      </Container>
-      )
+  return loading ? (
+    <Loading fullHeight />
+  ) : (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          alignItems: 'center',
+          flexDirection: 'column',
+        }}
+      >
+        <img src={Assets.Images.logo} alt="logo" style={{ width: '100px' }} />
+        <Outlet />
+      </Box>
+    </Container>
   );
 };
 
